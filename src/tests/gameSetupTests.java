5767f7a026d13009fe96fd738d84afe777e25de8
Kai Mizuno <Mizuno.PK@gmail.com>
2019-10-24 09:20:31 -0600

    Created nessisary classes for Clue Player Assignment. (Card, CardType, ComputerPlayer, Player, HumanPlayer, Solution, gameSetupTests

---
+++ src/tests/gameSetupTests.java
@@ -0,0 +1,26 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class gameSetupTests {
+	
+	@BeforeClass
+	public void setup() {
+		
+	}
+	
+	@Before
+	public void setUpTest() {
+		
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
