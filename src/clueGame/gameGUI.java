52535900d4cabab590ab3e148c3befbecf774bde
Jensen Eicher <jeicher@mymail.mines.edu>
2019-11-01 16:10:09 -0600

    Finished GUI part 1

--- src/clueGame/gameGUI.java
+++ src/clueGame/gameGUI.java
@@ -4,12 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 import javax.swing.JButton;import javax.swing.JFrame;import javax.swing.JLabel;
 import javax.swing.JPanel;import javax.swing.JTextField;import javax.swing.border.EtchedBorder;
+import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 
 public class gameGUI extends JPanel {
-    private JTextField name;
+    //TEST VARIABLES ONLY FOR SHOWING DISPLAY
+    private String name = "Mark";
+    private int dieRoll = 3;
+    private String guessResp = "No";
+
 
     public gameGUI(){
+        //set up each panel and arrange them in correct order
         setLayout(new GridLayout(2,1));
         JPanel panel1 = middlePanel();
         add(panel1);
@@ -21,45 +27,63 @@ public class gameGUI extends JPanel {
     private JPanel middlePanel() {
         JPanel panel = new JPanel();
         // Use a grid layout, 1 row, 2 elements (label, text)
-        panel.setLayout(new GridLayout(1,3));
-        JLabel nameLabel = new JLabel("Whose Turn?");
-        panel.add(nameLabel);
-
-        // no layout specified, so this is flow
+        panel.setLayout(new GridLayout(1,4));
+        //Create label for displaying whose turn and their name
+        JLabel Label = new JLabel("<html> Whose turn? &nbsp" + "<br>" + name + "<html>");
+        panel.add(Label);
+        //set boarder layout for formatting
+        Label.setBorder(new TitledBorder (new EtchedBorder(), ""));
+        // Create a button for next player (will add listener)
         JButton nextPlayer = new JButton("Next Player");
+        //Create a button for making an accusation
         JButton makeAccusation = new JButton("Make an Accusation");
         panel.setBorder(new TitledBorder (new EtchedBorder(), ""));
         panel.add(nextPlayer);
         panel.add(makeAccusation);
+        //Create layout for easy formatting
+        FlowLayout layout = new FlowLayout();
+        layout.setHgap(50);
+        layout.setVgap(10);
+        panel.setLayout(layout);
         return panel;
     }
 
     private JPanel bottomPanel() {
         JPanel panel = new JPanel();
-        // Use a grid layout, 1 row, 2 elements (label, text)
-        panel.setLayout(new GridLayout(1,3));
-        JLabel nameLabel = new JLabel("Die Roll: ");
-        panel.add(nameLabel);
-        panel.setBorder(new TitledBorder (new EtchedBorder(), ""));
-        // no layout specified, so this is flow
-        JTextField guess = new JTextField("Guess: ");
+        // Use a grid layout, 1 row, 6 elements (label, text)
+        panel.setLayout(new GridLayout(1,6));
+        JLabel die = new JLabel("Die Roll: " + dieRoll + " ");
+        panel.add(die);
+        //set a boarder for die
+        die.setBorder(new TitledBorder (new EtchedBorder(), ""));
+        // Get Guess from user
+        JTextField guess = new JTextField("Enter Your Guess Here");
         panel.add(guess);
-        JLabel guessResponse = new JLabel("Guess Response: ");
-        panel.add(guessResponse);
-        panel.setMaximumSize( new Dimension(10,15));
+        //Set a boarder for guess
+        guess.setBorder(new TitledBorder (new LineBorder(Color.BLACK), "Guess"));
+        // Display Response
+        JLabel guessR = new JLabel("<html> Guess Response: &nbsp" + "<br>" + guessResp + " <html>");
+        panel.add(guessR);
+        //set boarder for Guess Response
+        guessR.setBorder(new TitledBorder (new EtchedBorder(), ""));
+        //Layout for easy formatting
+        FlowLayout layout = new FlowLayout();
+        layout.setHgap(50);
+        layout.setVgap(10);
+        panel.setLayout(layout);
         return panel;
     }
 
     public static void main(String[] args) {
-        // Create a JFrame with all the normal functionality
+        // Create a JFrame
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Clue");
-        frame.setSize(700, 200);
+        frame.setSize(550, 175);
         // Create the JPanel and add it to the JFrame
         gameGUI gui = new gameGUI();
         frame.add(gui, BorderLayout.CENTER);
-        // Now let's view it
+        // show it
         frame.setVisible(true);
     }
 
